094b3809fcfe4f011094a58755cf80c34b205e0a
root <root@DESKTOP-6B3FQL8.localdomain>
2022-07-06 11:47:22 -0500

    fixed some of the function and did some of the stretch goals

--- Functions/apps.js
+++ Functions/apps.js
@@ -6,14 +6,15 @@
 // Use the if-then-else construct 
 // available in Javascript.
 
-function max(){
+function max() {
     let num1 = 15;
     let num2 = 20;
-if (num1 > num2)
-console.log(num1);
-else{
-    (num2 > num1)
-    console.log(num2);}
+    if (num1 > num2)
+        console.log(num1);
+    else {
+        (num2 > num1)
+        console.log(num2);
+    }
 }
 max()
 
@@ -25,18 +26,21 @@ max()
 // three numbers as arguments and returns 
 // the largest of them.
 
-function maxOfThree(){
+function maxOfThree() {
     let num1 = 25;
     let num2 = 55;
     let num3 = 12;
 
-    if(num1 > num2 && num1 > num3){
-    console.log(num1);}
+    if (num1 > num2 && num1 > num3) {
+        console.log(num1);
+    }
     else if
-        (num2 > num1 && num2 > num3)
-        {console.log(num2)}
-    else
-        {console.log(num3);}
+        (num2 > num1 && num2 > num3) { 
+            console.log(num2);
+        }
+    else { 
+        console.log(num3);
+     }
 }
 maxOfThree()
 
@@ -47,19 +51,29 @@ maxOfThree()
 // Write a function that takes a character 
 // (i.e. a string of length 1) and returns true if it is a 
 // vowel, false otherwise.
+let letter = 'u';
+function vowel() {
 
-function vowel(){
-    let vowels = ['a','e','i','o','u'];
-    let letter = 'a';
-    for(let i = 0; i < vowels.length; i++){
-        if(letter = vowels){
-        console.log(true)
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let i = 0; i < vowels.length; i++) {
+        // test to see is letter is not equal to vowel
+        if (letter != vowels[i]) {
+            // we continue to loop until it returns as false 
+            continue;
+        }
+        // if letter is not equal to vowel it will loop again
+        return true;
     }
-    else{console.log(false);
-    }
-    }
+    // if letter is equal to vowel loop stop
+    return false;
 }
-vowel()
+// test to see whatever letter is equal to a vowel
+if (vowel(letter)) {
+    console.log(`${letter} is a vowel`);
+} else {
+    console.log(`${letter} is not a vowel`);
+}
 
 
 // ### Exercise 4 
@@ -69,11 +83,11 @@ vowel()
 //  For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) 
 //  should return 24.
 
-function sumAndMultiply(){
-    let numsArr = [1,2,3,4]
+function sumAndMultiply() {
+    let numsArr = [1, 2, 3, 4]
     let sum = 0;
     let multiply = 1;
-    for(let i = 0; i <numsArr.length; i++){
+    for (let i = 0; i < numsArr.length; i++) {
         sum += numsArr[i];
         multiply *= numsArr[i];
     }
@@ -92,8 +106,43 @@ sumAndMultiply();
 // Define a function reverse() that computes the reversal of a string. For example, 
 // reverse("jag testar") should return the string "ratset gaj".
 
-function random(){
-    let arr = ["reverse", "this", "will"];
-        console.log(arr.reverse())
+let str = 'will this reverse';
+
+function reverseString() {
+
+    //splits the string into one letter each
+    let splitString = str.split('');
+
+    // reverses each letter from right to left
+    let reverseArray = splitString.reverse('');
+
+    // joins the string back together
+    let joinArray = reverseArray.join('');
+
+    return joinArray;
 }
-random()
+console.log(reverseString())
+
+
+
+// ### Stretch Goal Exercise 1
+
+// Write a function `lengths` that accepts a single 
+// parameter as an argument, namely an array of strings. 
+// The function should return an array of numbers where each 
+// number is the length of the corresponding string.
+
+// ["hello", "what", "is", "up", "dude"]; => 
+// [5, 4, 2, 2, 4]
+
+let words = ["hello", "what", "is", "up", "dude"];
+
+function lengths() {
+    let myArray = [];
+    for (i = 0; i < words.length; i++) {
+        myArray.push(words[i].length);
+    }
+    console.log(myArray);
+    return myArray;
+}
+lengths();
--- Loops/app.js
+++ Loops/app.js
@@ -1,8 +1,25 @@
 // Unless stated otherwise, your code should go _only_ inside the anonymous function below.
 (function () {
 
+const apples = [1, 3, 5, 7, 9];
+
+const bakers = [9, 0, 3, 8, 4, 2, 6, 1, 7, 5];
+
+const cards = [
+  1, 0, 8, 14, 10, 6, 4, 22, 15, 24, 15, 13, 13, 5, 0, 25, 21, 24, 24, 17, 4,
+  19, 15, 19, 9,
+];
+
+const dogs = [
+  29, 29, 17, 23, 23, 29, 17, 29, 17, 29, 23, 29, 23, 23, 29, 29, 23, 23, 17,
+  23, 29, 29, 23, 29, 17,
+];
+
+const eggs = [81, 75, 75, 81, 32, 75, 81, 75, 81];
+
+
   // 1. Show array apples.
-  const apples = [1, 3, 5, 7, 9];
+  
   console.log(apples);
 
   // 2. Show how many elements are in array apples.
@@ -18,7 +35,7 @@
 
 
   // 4. Show array bakers.
-  const bakers = [9, 0, 3, 8, 4, 2, 6, 1, 7, 5];
+
   console.log(bakers);
 
   // 5. Calculate the sum of the numbers in array bakers.
@@ -47,16 +64,12 @@
       evens = evens + bakers[i];
     }
   }
-  
+
   // show the final result
   console.log(evens)
 
 
   // 7. Show array cards.
-  const cards = [
-    1, 0, 8, 14, 10, 6, 4, 22, 15, 24, 15, 13, 13, 5, 0, 25, 21, 24, 24, 17, 4,
-    19, 15, 19, 9,
-  ];
   console.log(cards);
 
   // 8. Determine *if* 15 is in array cards, and display the answer.
@@ -79,19 +92,19 @@
   // show the final result
   for (let i = 0; i < cards.length; i++) {
     if (cards[i] === 15) {
-     console.log(`Number 15 appears on card numbers ${[i]}`); 
+      console.log(`Number 15 appears on card numbers ${[i]}`);
     }
   }
 
   // 10. How many times does 15 appear in array cards?
-let fifteen = cards.filter(card => card === 15);
+  let fifteen = cards.filter(card => card === 15);
 
   // show the final result
-console.log(`15 appears ${fifteen.length} times`);
+  console.log(`15 appears ${fifteen.length} times`);
 
   // 11. How many times does 0, 4 any 13 appear in array cards?
   let number = cards.filter(card => card === 0 || card === 4 || card === 13);
-  
+
   // show the final result
   console.log(`The numbers 0, 4, and 13 together show up ${number.length} times combined`);
 
@@ -102,25 +115,21 @@ console.log(`15 appears ${fifteen.length} times`);
   let firstNum = cards.indexOf(15);
 
   //add 1 to firstIndex b/c second param of indexOf is index number where we wanna begin search, we wanna start looking after the first instance
-  let secondNum = cards.indexOf(15, (firstNum +1));
-  let thirdNum = cards.indexOf(15, (secondNum +1));
-  positions.push(firstNum,' ' +  secondNum,' ' + thirdNum);
-  
+  let secondNum = cards.indexOf(15, (firstNum + 1));
+  let thirdNum = cards.indexOf(15, (secondNum + 1));
+  positions.push(firstNum, ' ' + secondNum, ' ' + thirdNum);
+
   // show the final result
   console.log(`These positions in the cards array hold a 15: ${positions}`);
 
   // 13. How many numbers are in array cards are even?
-let evemNumbers = cards.filter(card => {
-  return card % 2 ===0;
-})
+  let evemNumbers = cards.filter(card => {
+    return card % 2 === 0;
+  })
   // show the final result
-console.log(`There are ${evemNumbers.length} even numbers`)
+  console.log(`There are ${evemNumbers.length} even numbers`)
 
   // 14. Show array dogs.
-  const dogs = [
-    29, 29, 17, 23, 23, 29, 17, 29, 17, 29, 23, 29, 23, 23, 29, 29, 23, 23, 17,
-    23, 29, 29, 23, 29, 17,
-  ];
   console.log(dogs);
 
   // 15. How large is array dogs?
@@ -128,37 +137,45 @@ console.log(`There are ${evemNumbers.length} even numbers`)
 
   // 16. Calculate whether there are more than 8 29s in array dogs.
   let nums = dogs.filter(dog => dog === 29);
-  if(nums.length > 8){
+  if (nums.length > 8) {
     console.log('There are more than 8 29s in array dogs.');
-  }else{
+  } else {
     console.log('There are less than 8 29s in array dogs.');
   }
 
   // 17. Calculate whether there are more than 20 17s and 23s combined.
   let total = dogs.filter(dog => dog === 17 || dog === 23);
-  if(total.length > 20){
+  if (total.length > 20) {
     console.log('There are more than 20 17s and 23s combined.');
-  }else{
+  } else {
     console.log('There are less than 20 17s and 23s combined.');
   }
 
   // 18. Calculate how many 29s are in array dogs.
-console.log(`There are ${nums.length} 29s in the dogs array `)
+  console.log(`There are ${nums.length} 29s in the dogs array `)
 
   // 19. Calculate how many 23s and 17s combined are in array dogs.
-console.log(`There are ${total.length} 17s and 23s combined`)
+  console.log(`There are ${total.length} 17s and 23s combined`)
 
 
   // YOU CAN STOP HERE -- July 5th, 2022
   // 20. Show array eggs.
-  const eggs = [81, 75, 75, 81, 32, 75, 81, 75, 81];
+  console.log(eggs)
 
   // 21. How large is array eggs?
+  console.log(`The eggs array is ${eggs.length} items long`);
 
   // 22. How many 32s are in array eggs?
+  let numTotal = eggs.filter(egg => egg === 32);
+  if (numTotal.length < 32) {
+    console.log(`There are ${numTotal.length} 32(s) in this array`);
+  }
 
   // 23. Does array eggs have only 75s and 81s?
-
+  let combinedTotal = eggs.filter(egg => egg === 75 || egg === 81);
+  if (combinedTotal.length < 75) {
+    console.log(`There are ${combinedTotal.length} 75(s) and 81(s) in the eggs array`);
+  }
 
 })();
 
@@ -170,3 +187,16 @@ console.log(`There are ${total.length} 17s and 23s combined`)
 //  Start with an initial image of guy1.png then every .5 second (500 ms) 
 //  update the image to the next one in sequence. Once you reach image guy9.png 
 //  start over with the guy1.png image.
+
+// get the animation id childen from the html doc
+const frames = document.getElementById("animation").children;
+
+// let frameCount equal the amount of childen in the animation id 
+const frameCount = frames.length;
+
+let i = 0;
+
+setInterval(function (){
+  frames[i % frameCount].style.display = "none";
+  frames[++i % frameCount].style.display = "block";
+}, 500);
